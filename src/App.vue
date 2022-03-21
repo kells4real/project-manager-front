10f95fd39842b9c60a614fccb5bcbb93a999462f
Kells <kells4real@gmail.com>
2022-03-21 14:45:19 +0100

    Changed the logo

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,8 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="logo"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
         />
       </div>
@@ -85,7 +76,7 @@
 </template>
 
 <script>
-
+const image = require('./Edufy.png')
 export default {
   name: 'App',
 
@@ -98,6 +89,7 @@ export default {
        
         
       ],
+      logo: image
   }),
 };
 </script>
